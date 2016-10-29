dcf489645c7000f3a809b50dc2f34a2d13e7cb3c
KshipraKode <kshiprakode@gmail.com>
2016-10-29 15:43:17 -0400

    Updated Array_Strings_6

--- Array_Strings_6.java
+++ Array_Strings_6.java
@@ -1,8 +1,8 @@
 // Compress the given string depending on the number of characters.
 // If the compressed string does not have a length smaller than the 
 // given string, return the given string.
-// 
-
+// Since each character in the string needs to be visited atleast once
+// Time Complexity - O(n)
 
 package ctci.kshiprakode;
 
@@ -13,32 +13,35 @@ public class Array_Strings_6 {
 		String newString = new String();
 		
 		int counter = 1;
-		for(int i = 0 , j = 1 ;i < string.length();)
+		for(int i = 0 , j = 1 ;i < string.length() && j < string.length();)
 		{
-			if(i < string.length() && j <  string.length())
+			while(string.charAt(i) == string.charAt(j))
 			{
-				while(string.charAt(i) == string.charAt(j) && j < string.length())
-				{
-					counter++;
-					j++;
-				}
+				counter++;
+				j++;
+				if(j >= string.length())
+					break;
 			}
-
 			newString += Character.toString(string.charAt(i)) + counter;
 			i = j;
 			counter = 0;
-			System.out.println(newString);
 		}
-		
-		return newString;
-		
+
+		if(newString.length() < string.length())
+			return newString;
+
+		return string;
 	}
 	
 	public static void main(String[] args) {
+	
 		// TODO Auto-generated method stub
-		
-		System.out.println(stringCompression("aaa"));
-
+		System.out.println(stringCompression("aaaaaba"));
+		System.out.println(stringCompression("aba"));
+		System.out.println(stringCompression("aaabbbccccdddeef"));
+		System.out.println(stringCompression("aaaaaaabbbbbbb"));
+		System.out.println(stringCompression("abcdeffffffff"));
+		System.out.println(stringCompression("abcdef"));
+		System.out.println(stringCompression(""));
 	}
-
 }
