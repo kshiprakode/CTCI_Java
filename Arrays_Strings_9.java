2b52c4c3af802e6fb0bc4d5e2d3b1cba2c5367ba
KshipraKode <kshiprakode@gmail.com>
2016-11-01 20:42:25 -0400

    Added Arrays_Strings_9, isRotation

---
+++ Arrays_Strings_9.java
@@ -0,0 +1,26 @@
+// Assume you are given a method isSubstring that checks if one string is the substring of another
+// Write a method to check if one string is a rotation of the other with only one call is isSubstring
+
+package ctci.kshiprakode;
+
+public class Arrays_Strings_9 {
+
+
+	public static boolean isRotation(String str1, String str2)
+	{
+
+		str2 = str2 + str2;
+		return str2.indexOf(str1) == -1 ? false : true;
+	
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Is waterbottle a rotation of bottlewater  : " + isRotation("waterbottle","bottlewater"));
+		System.out.println("Is waterbottle a rotation of botwatetler  : " + isRotation("waterbottle","botwatetler"));
+		
+		
+	}
+
+}
